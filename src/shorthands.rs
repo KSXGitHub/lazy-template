a74bcda505f39505a6b5bfadc7bd80f71ccfc81b
khai96_ <hvksmr1996@gmail.com>
2024-10-05 04:23:39 +0700

    docs: `simple_curly_braces`

--- src/shorthands.rs
+++ src/shorthands.rs
@@ -1,21 +1,24 @@
 use crate::enclosed::{self, SimpleEnclosedTemplate};
 use pipe_trait::Pipe;
 
+/// Create a simple template of string interpolation with curly braces.
+///
+/// ```
+/// # #[cfg(not(feature = "std"))] fn main() {}
+/// # #[cfg(feature = "std")] fn main() {
+/// # use pretty_assertions::assert_eq;
+/// # use lazy_template::simple_curly_braces;
+/// let actual = simple_curly_braces()
+///     .to_string("foo = {FOO}; bar = {BAR}", |query| match query.as_str() {
+///         "FOO" => Ok(123),
+///         "BAR" => Ok(456),
+///         _ => Err(()),
+///     })
+///     .unwrap();
+/// let expected = "foo = 123; bar = 456";
+/// assert_eq!(actual, expected);
+/// # }
+/// ```
 pub fn simple_curly_braces<'a>() -> SimpleEnclosedTemplate<'a> {
     enclosed::Parser::simple_curly_braces().pipe(SimpleEnclosedTemplate::new)
-}
-
-#[cfg(feature = "std")]
-#[test]
-fn test_simple_curly_braces() {
-    use pretty_assertions::assert_eq;
-    let actual = simple_curly_braces()
-        .to_string("foo = {FOO}; bar = {BAR}", |query| match query.as_str() {
-            "FOO" => Ok(123),
-            "BAR" => Ok(456),
-            _ => Err(()),
-        })
-        .unwrap();
-    let expected = "foo = 123; bar = 456";
-    assert_eq!(actual, expected);
 }
