bce7173fbe753ad046475ebaf6fbb238f2362851
khai96_ <hvksmr1996@gmail.com>
2024-10-05 21:31:37 +0700

    feat(api): rename a struct

--- src/enclosed/segment.rs
+++ src/enclosed/segment.rs
@@ -2,26 +2,31 @@ use crate::Render;
 use derive_more::Display;
 use pipe_trait::Pipe;
 
+/// Represent a segment of a parsed template.
 #[derive(Debug, Clone, Copy)]
 pub enum Segment<Query> {
     Character(char),
     Expression(Query),
 }
 
+/// Returned upon the [rendering](Render) of a [`Segment`].
+///
+/// Value of this type can be converted to a string by using the [`Display`] trait.
 #[derive(Debug, Display, Clone, Copy)]
-pub enum SegmentOutput<Output> {
+pub enum SegmentDisplay<Output> {
     Character(char),
     ExpressionResult(Output),
 }
 
-impl<Respond, Output, Error, Query> Render<Respond, SegmentOutput<Output>, Error> for Segment<Query>
+impl<Respond, Output, Error, Query> Render<Respond, SegmentDisplay<Output>, Error>
+    for Segment<Query>
 where
     Respond: FnMut(Query) -> Result<Output, Error>,
 {
-    fn render(self, respond: &mut Respond) -> Result<SegmentOutput<Output>, Error> {
+    fn render(self, respond: &mut Respond) -> Result<SegmentDisplay<Output>, Error> {
         Ok(match self {
-            Segment::Character(value) => SegmentOutput::Character(value),
-            Segment::Expression(query) => respond(query)?.pipe(SegmentOutput::ExpressionResult),
+            Segment::Character(value) => SegmentDisplay::Character(value),
+            Segment::Expression(query) => respond(query)?.pipe(SegmentDisplay::ExpressionResult),
         })
     }
 }
