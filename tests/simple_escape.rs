1476a24b15a20ea5914a1dacd3ea18da8b8c00cf
khai96_ <hvksmr1996@gmail.com>
2024-10-06 01:29:16 +0700

    test: special characters

---
+++ tests/simple_escape.rs
@@ -0,0 +1,22 @@
+#![cfg(feature = "std")]
+use lazy_template::{
+    enclosed::{Parser, SimpleEscapeParser, SimpleQuery, SimpleQueryParser},
+    IntoTemplateSystem,
+};
+use pretty_assertions::assert_eq;
+use std::convert::Infallible;
+
+#[test]
+fn make_special_characters() {
+    let actual = Parser::curly_braces()
+        .with_escape_parser(SimpleEscapeParser)
+        .with_query_parser(SimpleQueryParser)
+        .into_template_system::<SimpleQuery>()
+        .lazy_parse(r"special characters: \\, \0, \b, \n, \r, \t")
+        .to_string(Ok::<&str, Infallible>)
+        .unwrap();
+    dbg!(&actual);
+    let expected = "special characters: \\, \0, \u{8}, \n, \r, \t";
+    dbg!(expected);
+    assert_eq!(actual, expected);
+}
