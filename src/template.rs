0592ef5f7c660bb5f041e6f4d0fcb7e62fa582a8
khai96_ <hvksmr1996@gmail.com>
2024-10-06 01:50:03 +0700

    refactor: move `to_string` to `std_extensions`

--- src/template.rs
+++ src/template.rs
@@ -26,22 +26,6 @@ pub enum TemplateApplicationError<ParseError, QueryError, WriteError> {
 }
 
 impl<SegmentResultIter, Query> Template<SegmentResultIter, Query> {
-    #[cfg(feature = "std")]
-    pub fn to_string<Segment, ParseError, RenderOutput, QueryOutput, QueryError, Respond>(
-        self,
-        respond: Respond,
-    ) -> Result<String, TemplateApplicationError<ParseError, QueryError, fmt::Error>>
-    where
-        SegmentResultIter: Iterator<Item = Result<Segment, ParseError>>,
-        RenderOutput: fmt::Display,
-        Segment: Render<Respond, RenderOutput, QueryError>,
-        Respond: FnMut(Query) -> Result<QueryOutput, QueryError>,
-    {
-        let mut buf = String::new();
-        self.write_to(&mut buf, respond)?;
-        Ok(buf)
-    }
-
     pub fn write_to<Output, Segment, ParseError, RenderOutput, QueryOutput, QueryError, Respond>(
         self,
         output: &mut Output,
--- src/template/std_extensions.rs
+++ src/template/std_extensions.rs
@@ -1,11 +1,25 @@
 #![cfg(feature = "std")]
 use super::{Template, TemplateApplicationError};
 use crate::Render;
-use core::fmt::Display;
+use core::fmt;
 use pipe_trait::Pipe;
 use std::io;
 
 impl<SegmentResultIter, Query> Template<SegmentResultIter, Query> {
+    pub fn to_string<Segment, ParseError, RenderOutput, QueryOutput, QueryError, Respond>(
+        self,
+        respond: Respond,
+    ) -> Result<String, TemplateApplicationError<ParseError, QueryError, fmt::Error>>
+    where
+        SegmentResultIter: Iterator<Item = Result<Segment, ParseError>>,
+        RenderOutput: fmt::Display,
+        Segment: Render<Respond, RenderOutput, QueryError>,
+        Respond: FnMut(Query) -> Result<QueryOutput, QueryError>,
+    {
+        let mut buf = String::new();
+        self.write_to(&mut buf, respond)?;
+        Ok(buf)
+    }
     pub fn to_writer<Writer, Segment, ParseError, RenderOutput, QueryOutput, QueryError, Respond>(
         self,
         writer: &mut Writer,
@@ -14,7 +28,7 @@ impl<SegmentResultIter, Query> Template<SegmentResultIter, Query> {
     where
         Writer: io::Write,
         SegmentResultIter: Iterator<Item = Result<Segment, ParseError>>,
-        RenderOutput: Display,
+        RenderOutput: fmt::Display,
         Segment: Render<Respond, RenderOutput, QueryError>,
         Respond: FnMut(Query) -> Result<QueryOutput, QueryError>,
     {
