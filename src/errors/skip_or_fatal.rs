5b03d1245f24ab38977eaa6afed332ebf1b337ad
khai96_ <hvksmr1996@gmail.com>
2024-10-05 20:36:04 +0700

    refactor: move deeper

---
+++ src/errors/skip_or_fatal.rs
@@ -0,0 +1,24 @@
+/// Result type of [`IntoSkipOrFatal::into_skip_or_fatal`].
+pub enum SkipOrFatal<Skip, Fatal> {
+    Skip(Skip),
+    Fatal(Fatal),
+}
+
+/// Trait of error type of a component parser.
+/// It checks whether the error means "skip" or "fatal".
+pub trait IntoSkipOrFatal {
+    /// "Skip" means that the parent parser may either try to parse the next type of component or error,
+    type Skip;
+    /// "Fatal" means that the parent parser should bail immediately.
+    type Fatal;
+    /// Check whether the error returned from a component parser is skip or fatal.
+    fn into_skip_or_fatal(self) -> SkipOrFatal<Self::Skip, Self::Fatal>;
+}
+
+impl<Skip, Fatal> IntoSkipOrFatal for SkipOrFatal<Skip, Fatal> {
+    type Skip = Skip;
+    type Fatal = Fatal;
+    fn into_skip_or_fatal(self) -> Self {
+        self
+    }
+}
