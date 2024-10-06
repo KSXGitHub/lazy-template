5ec0336982756fb43f6c733f17797f0628242b28
khai96_ <hvksmr1996@gmail.com>
2024-10-06 23:32:23 +0700

    test: simplify `rust_version::sync`

--- tests/rust_version.rs
+++ tests/rust_version.rs
@@ -1,19 +1,13 @@
 #![cfg(feature = "std")]
-use cargo_metadata::MetadataCommand;
-use cargo_toml::Manifest;
 use pipe_trait::Pipe;
 use pretty_assertions::assert_eq;
 use semver::Version;
-use std::fs;
+use std::{fs, path::Path};
 
 #[test]
 fn sync() {
-    let ws_root = MetadataCommand::new()
-        .exec()
-        .expect("the command `cargo metadata` should execute successfully")
-        .workspace_root;
-    dbg!(&ws_root);
-    let rust_toolchain = ws_root
+    let rust_toolchain = env!("CARGO_MANIFEST_DIR")
+        .pipe(Path::new)
         .join("rust-toolchain")
         .pipe(fs::read_to_string)
         .expect("the rust-toolchain file should be readable")
@@ -22,18 +16,7 @@ fn sync() {
         .expect("the content rust-toolchain should be valid semver");
     dbg!(&rust_toolchain);
     let Version { major, minor, .. } = &rust_toolchain;
-    let rust_version = ws_root
-        .join("Cargo.toml")
-        .pipe(Manifest::from_path)
-        .expect("Cargo.toml should be readable")
-        .package
-        .expect("package should be defined")
-        .rust_version
-        .expect("rust_version should be defined");
-    let rust_version = rust_version
-        .get()
-        .expect("rust-version should be valid")
-        .as_str();
+    let rust_version = env!("CARGO_PKG_RUST_VERSION");
     dbg!(rust_version);
     assert_eq!(rust_version, format!("{major}.{minor}"));
 }
